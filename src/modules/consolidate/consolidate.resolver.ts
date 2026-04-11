c410a4c99f0543254c49d186998b1b33060e76cd
LudwinGit <ludwinburrion@gmail.com>
2026-04-10 18:01:12 -0600

    bug: findall consolidates

--- src/modules/consolidate/consolidate.resolver.ts
+++ src/modules/consolidate/consolidate.resolver.ts
@@ -69,12 +69,14 @@ export class ConsolidateResolver {
 
   @ResolveField(() => Seller, { nullable: true })
   async seller(@Parent() consolidate: Consolidate): Promise<Seller> {
+    if (consolidate.seller) return consolidate.seller;
     const { seller } = await this.consolidateService.find(consolidate.id);
     return seller;
   }
 
   @ResolveField(() => Route, { nullable: true })
   async route(@Parent() consolidate: Consolidate): Promise<Route> {
+    if (consolidate.route) return consolidate.route;
     const { route } = await this.consolidateService.find(consolidate.id);
     return route;
   }
